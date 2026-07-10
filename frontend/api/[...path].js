8aa09f3141350ef589370707ad3356e712a33b5d
makosmalikos <makosmalikos@gmail.com>
2026-07-10 13:00:28 +0500

    Delete frontend directory

--- frontend/api/[...path].js
+++
@@ -1,6 +0,0 @@
-// Vercel serverless-функция: все /api/* запросы идут в Express-приложение.
-import { createApp } from '../server/app.js';
-const app = createApp();
-export default function handler(req, res) {
-  return app(req, res);
-}
